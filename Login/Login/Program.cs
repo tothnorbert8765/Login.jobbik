bdd1bc52a26610a1621a99e432185b0e13b59f9f
tothnorbert8765 <43171740+tothnorbert8765@users.noreply.github.com>
2022-09-16 12:33:53 +0200

    Add files via upload

---
+++ Login/Login/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Login
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Felhasználó név:");
+            string felh = Console.ReadLine();
+            Console.WriteLine("Jelszó");
+            string jelsz = Console.ReadLine();
+            string a = felh + ";" + jelsz;
+            string[] beolvas = File.ReadAllLines("login.txt");
+            bool OK = false;
+            for (int i = 0; i < beolvas.Length && !OK; i++)
+            {
+                if (a==beolvas[i])
+                {
+                    Console.WriteLine("Sikeres bejelentkezés");
+                    OK = true;
+                }
+                
+
+            }
+            if (OK==true)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Hibás felhasználónév vagy jelszó");
+            }
+            Console.ReadKey();
+        }
+    }
+}
